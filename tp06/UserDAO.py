891ab42093d80a85c40355bfecd873f066a520d1
Frédéric GAURAT <fgaurat@gmail.com>
2023-11-29 15:25:25 +0100

    add full dao

--- tp06/UserDAO.py
+++ tp06/UserDAO.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from User import User
 class UserDAO:
 
     def __init__(self,db_file) -> None:
@@ -7,7 +7,16 @@ class UserDAO:
 
     
     def findAll(self):
+        users = []
         cur = self.__con.cursor()
+        sql="SELECT * FROM users_tbl"
+        rs = cur.execute(sql)
+        for row in rs.fetchall():
+            u = User(*row)
+            users.append(u)
 
+        return users
+    
+    
     def __del__(self):
         self.__con.close()
--- tp06/main.py
+++ tp06/main.py
@@ -1,10 +1,12 @@
+from UserDAO import UserDAO
+
 def main():
     dao = UserDAO('./tp06/users_db.db')
 
     users = dao.findAll()
     for user in users:
-        print(user.name,user.email)
+        print(user.last_name,user.email)
 
-        
+
 if __name__=='__main__':
     main()
